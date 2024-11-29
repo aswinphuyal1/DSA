7f19b394b776297ab74d6a2773ad222404fa63b0
aswinphuyal1 <phuyalaswin651@gmail.com>
2024-11-29 11:56:56 +0545

    1st
    1st

---
+++ 1st.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{char ch;
+
+    cout<<"enter a charecter in lower or upper case"<<endl;
+cin>>ch;
+    if(ch>='a' && ch<='z')
+    {
+        cout<<"it is ni lower case";
+    }
+    else 
+    {
+        cout<<"it is in uppercase ";
+    };
+}

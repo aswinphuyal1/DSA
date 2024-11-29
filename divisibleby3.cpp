11fab6daaf4443010852690ba54b27d326e1d8ef
aswinphuyal1 <phuyalaswin651@gmail.com>
2024-11-29 12:28:10 +0545

    day 2

---
+++ divisibleby3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int d;
+int i=0;
+    cout<<"Enter the number to you want"<<endl;
+   cin>>d;
+    for(i=0;i<=d;i++)
+    {
+        if(i%3==0)
+        {
+            cout<<"The number is"<<i<<endl;
+
+        }
+    
+    }
+}

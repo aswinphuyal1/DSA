3b0da7505fef456c914db88db527f5e44b425356
aswinphuyal1 <phuyalaswin651@gmail.com>
2024-12-04 16:35:53 +0545

    unique element in array

---
+++ uniques/valu/arreay.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,3,1,2,3,4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unique elements: ";
+    for (int i = 0; i < size; i++) {
+        int isUnique = 1;
+
+        for (int j = 0; j < size; j++) {
+            if (i != j && arr[i] == arr[j]) {
+                isUnique = 0; // Element is not unique
+            }
+        }
+
+        if (isUnique) {
+            cout << arr[i] << " ";
+        }
+    }
+
+    return 0;
+}
